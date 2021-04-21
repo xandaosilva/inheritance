c6244fc104be0fcdd7ec901d06598b446868a6d4
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-21 09:55:27 -0300

    Add SavingsAccount class

---
+++ Inheritance/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void updateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
